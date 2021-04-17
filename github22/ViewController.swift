da488d0892ff3e18010809cdd317e50f030ec4a7
rickygh12 <81440077+rickygh12@users.noreply.github.com>
2021-04-17 10:10:42 -0500

    Initial Commit

---
+++ github22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github22
+//
+//  Created by rvcstudent on 4/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
